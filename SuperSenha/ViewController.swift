b3f2dee1c283bcf811f839e3a5172a9e0da4defa
Lucas Bighi <lucasmbighi@live.com>
2018-12-02 15:55:29 -0200

    Initial Commit

---
+++ SuperSenha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperSenha
+//
+//  Created by Lucas Marques Bighi on 02/12/2018.
+//  Copyright © 2018 Lucas Marques Bighi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
